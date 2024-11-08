5d10ec0a67734990a0e7971b73d6afff1b225e13
v1nny:) <vinicioolivindo81@gmail.com>
2024-11-07 21:26:08 -0300

    teste3.0

--- main.c
+++ main.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int main()
+{
 
     int tamVetor;
     scanf("%d", &tamVetor);
 
     int *vetor = (int *)malloc(tamVetor * sizeof(int));
 
-    if(vetor == NULL){
+    if (tamVetor == 0)
+    {
         printf("[vazio]");
     }
-
-    for(int i = 0; i < tamVetor; i++){
-        scanf("%d", &vetor[i]);
-    }
-
-    
-    printf("[");
-    for(int l = 0; l < tamVetor; l++){
-        printf("%d", vetor[l]);
-        if(l < tamVetor-1){
-            printf(", ");
+    else
+    {
+        for (int i = 0; i < tamVetor; i++)
+        {
+            scanf("%d", &vetor[i]);
         }
-    }
-    printf("]");
 
-    free(vetor);
+        printf("[");
+        for (int l = 0; l < tamVetor; l++)
+        {
+            printf("%d", vetor[l]);
+            if (l < tamVetor - 1)
+            {
+                printf(", ");
+            }
+        }
+        printf("]");
+
+        free(vetor);
+    }
 
     return 0;
 }

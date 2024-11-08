48a231fc9dcd188fc64ff9aa2387eaf5e99913b7
v1nny:) <vinicioolivindo81@gmail.com>
2024-11-07 21:20:17 -0300

    teste

--- main.c
+++ main.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+    int tamVetor;
+    scanf("%d", &tamVetor);
+
+    int *vetor = (int *)malloc(tamVetor * sizeof(int));
+
+    if(vetor == NULL){
+        printf("[vazio]");
+    }
+
+    for(int i = 0; i < tamVetor; i++){
+        scanf("%d", &vetor[i]);
+    }
+
+    
+    printf("[");
+    for(int l = 0; l < tamVetor; l++){
+        printf("%d", vetor[l]);
+        if(l < tamVetor-1){
+            printf(",");
+        }
+    }
+    printf("]");
+
+    free(vetor);
 
     return 0;
 }
